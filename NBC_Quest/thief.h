99f6b7e92a12185cad9a046bc56e18a6ab4156cb
Jungbin Choi <j1b2c3@naver.com>
2026-03-04 14:05:46 +0900

    초기 커밋.

---
+++ NBC_Quest/thief.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "player.h"
+
+class Thief : public Player{
+public:
+    Thief(string nickname);
+    void attack() override;
+    void attack(Monster* monster) override;
+};
---
+++ NBC_Quest/warrior.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "player.h"
+
+class Warrior : public Player{
+public:
+    Warrior(string nickname);
+    void attack() override;
+    void attack(Monster* monster) override;
+
+};
